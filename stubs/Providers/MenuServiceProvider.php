11b3630b571bed5f442783a3a331c305fbd04526
Md. Raihan Uddin Ahamed Chowdhury <chowdhuryraihan07@gmail.com>
2021-03-01 21:13:07 +0600

    Check Menu Data exists

--- stubs/Providers/MenuServiceProvider.php
+++ stubs/Providers/MenuServiceProvider.php
@@ -23,13 +23,15 @@ class MenuServiceProvider extends ServiceProvider
    */
   public function boot()
   {
-    // get all data from menu.json file
-    $verticalMenuJson = file_get_contents(base_path('resources/data/menu-data/verticalMenu.json'));
-    $verticalMenuData = json_decode($verticalMenuJson);
-    $horizontalMenuJson = file_get_contents(base_path('resources/data/menu-data/horizontalMenu.json'));
-    $horizontalMenuData = json_decode($horizontalMenuJson);
+    if(file_exists(base_path('resources/data/menu-data/verticalMenu.json'))){
+        // get all data from menu.json file
+        $verticalMenuJson = file_get_contents(base_path('resources/data/menu-data/verticalMenu.json'));
+        $verticalMenuData = json_decode($verticalMenuJson);
+        $horizontalMenuJson = file_get_contents(base_path('resources/data/menu-data/horizontalMenu.json'));
+        $horizontalMenuData = json_decode($horizontalMenuJson);
 
-    // Share all menuData to all the views
-    \View::share('menuData', [$verticalMenuData, $horizontalMenuData]);
+        // Share all menuData to all the views
+        \View::share('menuData', [$verticalMenuData, $horizontalMenuData]);
+      }
   }
 }
