99aa84e88d420c0bc4656369b030452ff96eba84
Md. Raihan Uddin Ahamed Chowdhury <chowdhuryraihan07@gmail.com>
2021-03-01 12:18:20 +0600

    add files to composer

--- src/HerouiServiceProvider.php
+++ src/HerouiServiceProvider.php
@@ -19,6 +19,33 @@ class HerouiServiceProvider extends ServiceProvider
         }
         copy(__DIR__.'/../stubs/Helpers/helpers.php', app_path('Helpers/helpers.php'));
 
+        copy(base_path('composer.json'), __DIR__.'\composer.php');
+        $composer_string = file_get_contents(__DIR__.'\composer.php');
+        $composer_new_array = array();
+        $composer_array = json_decode($composer_string, true);
+
+        $autoload = array();
+        $can_replace_composer_file = 0;
+        if(isset($composer_array['autoload'])){
+            $composer_autoload = $composer_array['autoload'];
+            if(isset($composer_autoload['files'])){
+                $autoload['files'] = $composer_autoload['files'];
+                if(!in_array('app/Helpers/helpers.php', $autoload['files'])){
+                    $can_replace_composer_file = 1;
+                    $autoload['files'][count($autoload)] = 'app/Helpers/helpers.php';
+                }
+            } else {
+                $can_replace_composer_file = 1;
+                $autoload['files'][0] = 'app/Helpers/helpers.php';
+            }
+        }
+        if($can_replace_composer_file==1){
+            $composer_array['autoload']['files'] = $autoload['files'];
+            $composer_string = json_encode($composer_array, JSON_PRETTY_PRINT);
+            file_put_contents(__DIR__.'\composer.php', $composer_string);
+            copy(__DIR__.'\composer.php', base_path('composer.json'));
+        }
+
         if ($this->app->runningInConsole()) {
             $this->commands([
                 AuthCommand::class,
